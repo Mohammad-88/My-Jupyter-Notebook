d57d5af5bc16004338897642e82966aac5ac9c2f
Mohammad-88 <108989745+Mohammad-88@users.noreply.github.com>
2022-07-26 10:15:41 +0430

    Add files via upload

---
+++ My Jupyter notebook .py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+# **Mohammad Khoshkam**
+# ***
+# Engineer
+# ***
+# *I'm interested in Data Science because i believe it's a very Sexy Job and its the Future*
+# ***
+# ### below line contain a simple code for as peer-graded assignment
+# 
+
+# In[4]:
+
+
+Two=2
+three=3
+print(Two*three)
+
+
+# ## Horizental Rule
+# 
+# My Name is Mohammad
+# *******************
+# 
+# 
+# I'm like Data Science
+# -------
+# 
+# 
+# 
+# ## Numbred List
+# 
+#  1. Jupyter
+#  ..1. Jupyter Lab
+#  ..2. Jupyter Notebook
+#  2. R Studio
+#  3. Data Science
+#  
+#  ## Images
+#  Inline-style: 
+# ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+# 
